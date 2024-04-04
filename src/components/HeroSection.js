3a4e1e98a2acfea63d4cbac75181adcce73e8e18
Vakho Lomtadze <vakho@Vakhos-MacBook-Air.local>
2024-04-04 14:59:25 +0400

    Fine tuned design

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,8 +5,8 @@ const HeroSection = () => {
     return (
       <div className="hero-section">
         <h1 className="text-burgundy mb-10">
-        <span className="text-5xl font-pacifico">WeRemote!</span>
-          <span className="text-4xl font-inter"> and so should you!</span>
+        <span className="text-6xl font-pacifico">WeRemote!</span>
+          {/* <span className="text-4xl font-inter"> and so should you!</span> */}
         </h1>
         <div className="search-bar-wrapper">
           <input
--- src/components/JobListings.js
+++ src/components/JobListings.js
@@ -23,17 +23,18 @@ const JobListings = ({ onJobSelect }) => {
   const formatSalaryRange = (min, max) => `$${min.toLocaleString()} - $${max.toLocaleString()}`;
 
   return (
-    <div className="my-4 mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
-      {jobs.map((job) => (
-        <div key={job._id} 
-             className="bg-white shadow-lg rounded-lg p-6 border border-gray-200 cursor-pointer mb-4"
-             onClick={() => onJobSelect(job)}>
-          <h3 className="text-xl font-bold text-gray-800">{job.role}</h3>
-          <p className="text-gray-600">{job.company}</p>
-          <p className="text-gray-500">{formatSalaryRange(job.salaryRange.min, job.salaryRange.max)}</p>
-          <p className="text-sm text-gray-400">{formatDistanceToNow(new Date(job.datePosted)) + ' ago'}</p>
-          {/* Mapping descriptors if needed */}
-        </div>
+    <div className="my-2 mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
+        {jobs.map((job) => (
+            <div key={job._id} 
+                className="bg-white shadow-lg rounded-lg px-6 py-4 border border-gray-200 cursor-pointer mb-4 flex justify-between items-center"
+                onClick={() => onJobSelect(job)}>
+            <div>
+                <h3 className="text-lg font-bold text-gray-800">{job.role}</h3>
+                <p className="text-gray-600">{job.company}</p>
+                <p className="text-gray-500">{formatSalaryRange(job.salaryRange.min, job.salaryRange.max)}</p>
+            </div>
+            <p className="text-sm text-burgundy font-light italic">{formatDistanceToNow(new Date(job.datePosted)) + ' ago'}</p>
+            </div>
       ))}
     </div>
   );
