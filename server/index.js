668cb892cd527283a16c90827a335894232356ce
AceHotel33 <lomvax@gmail.com>
2024-04-18 15:59:02 +0400

    Backend connection from Elastic Beanstalk to MongoDB established

--- server/index.js
+++ server/index.js
@@ -6,28 +6,34 @@ const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client
 
 const app = express();
 app.use(cors());
-app.use(express.json()); // For parsing application/json
+app.use(express.json());
+
+const secretsClient = new SecretsManagerClient({ region: 'eu-north-1' });
+
+app.get('/', (req, res) => {
+  res.send('Server is healthy!');
+});
 
 const connectToDatabase = async () => {
     let uri;
+
+    console.log(`Environment: ${process.env.NODE_ENV}`);
     
     if (process.env.NODE_ENV === 'production') {
-        // Configuration for AWS Secrets Manager using SDK v3
-        const client = new SecretsManagerClient({ region: 'eu-north-1' });
         const secretName = "arn:aws:secretsmanager:eu-north-1:339713118500:secret:MONGODB_URI-gFq8Bz";
 
         try {
-            const data = await client.send(new GetSecretValueCommand({ SecretId: secretName }));
+            const data = await secretsClient.send(new GetSecretValueCommand({ SecretId: secretName }));
             if (data.SecretString) {
                 const secret = JSON.parse(data.SecretString);
                 uri = secret.MONGODB_URI;
             }
+            console.log(`Attempting to connect to MongoDB at URI: ${uri}`);
         } catch (error) {
             console.error("Error retrieving secret:", error);
             return;
         }
     } else {
-        // Use .env in development
         uri = process.env.MONGODB_URI;
     }
 
@@ -36,13 +42,12 @@ const connectToDatabase = async () => {
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error(`MongoDB connection error: ${err}`);
+        process.exit(1);
     }
 };
 
-// Call connectToDatabase function
 connectToDatabase();
 
-// Define Job Posting Schema
 const jobPostingSchema = new mongoose.Schema({
     role: String,
     company: String,
@@ -56,10 +61,8 @@ const jobPostingSchema = new mongoose.Schema({
     applicationLink: String,
 });
 
-// Create Mongoose model
 const JobPosting = mongoose.model('JobPosting', jobPostingSchema);
 
-// Define routes
 app.post('/api/jobs', async (req, res) => {
     try {
         const newJob = new JobPosting(req.body);
@@ -70,16 +73,17 @@ app.post('/api/jobs', async (req, res) => {
     }
 });
 
-app.get('/api/jobs', async (req, res) => {
+app.get('/test-db', async (req, res) => {
     try {
-        const jobPostings = await JobPosting.find().sort({ datePosted: -1 });
-        res.json(jobPostings);
+        const jobs = await JobPosting.find().limit(1);
+        console.log('Successfully fetched data from MongoDB:', jobs);
+        res.json(jobs);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        console.error('Error fetching data from MongoDB:', error);
+        res.status(500).json({ message: 'Failed to fetch data from MongoDB' });
     }
 });
 
-// Start the server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
